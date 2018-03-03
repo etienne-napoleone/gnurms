4261bf574e624e45d4364895f8bcd69459ae9c99
n0wastaken <n0@waifu.club>
2018-03-03 23:35:57 +0100

    🚿 clean up

--- gnurms.py
+++ gnurms.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
-import discord
-import asyncio
+#-*- coding: utf-8 -*-
 import re
 import os
 import sys
+import discord
 
 r = re.compile(r'(?:[^/]|^)[lL]inux')
 
-rms = "I'd just like to interject for a moment. What {} is referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called “Linux”, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\n\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called “Linux” distributions are really distributions of GNU/Linux."
-
-link = "🔗 https://www.gnu.org/gnu/gnu.html"
+rms = ("I'd just like to interject for a moment. What {} is referring to as "
+       "Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it"
+       ", GNU plus Linux. Linux is not an operating system unto itself, but "
+       "rather another free component of a fully functioning GNU system made "
+       "useful by the GNU corelibs, shell utilities and vital system "
+       "components comprising a full OS as defined by POSIX.\n\nMany "
+       "computer users run a modified version of the GNU system every day, "
+       "without realizing it. Through a peculiar turn of events, the version "
+       "of GNU which is widely used today is often called “Linux”, and many "
+       "of its users are not aware that it is basically the GNU system, "
+       "developed by the GNU Project.\n\nThere really is a Linux, and these "
+       "people are using it, but it is just a part of the system they use. "
+       "Linux is the kernel: the program in the system that allocates the "
+       "machine’s resources to the other programs that you run. The kernel "
+       "is an essential part of an operating system, but useless by itself; "
+       "it can only function in the context of a complete operating system. "
+       "Linux is normally used in combination with the GNU operating "
+       "system: the whole system is basically GNU with Linux added, or "
+       "GNU/Linux. All the so-called “Linux” distributions are really "
+       "distributions of GNU/Linux.\n\n🔗 https://www.gnu.org/gnu/gnu.html")
 
 client = discord.Client()
 
@@ -20,12 +37,12 @@ async def on_message(message):
         return
 
     if r.search(message.content):
-        msg = rms.format(message.author.mention) + "\n\n" + link
+        msg = rms.format(message.author.mention)
         try:
             await client.send_message(message.channel, msg)
             print("Someone got GNU/taught!")
         except Exception as e:
-            print(e)
+            print("Could not explain GNU/Linux to some plebs due to some errors")
 
 @client.event
 async def on_ready():
